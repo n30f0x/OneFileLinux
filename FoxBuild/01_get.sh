0e0abfd9573e9aa346de666781b17311bdac3228
n30f0x <87524177+n30f0x@users.noreply.github.com>
2023-04-25 10:03:28 +0300

    change variables

--- FoxBuild/01_get.sh
+++ FoxBuild/01_get.sh
@@ -1,12 +1,12 @@
 #/bin/bash
 
-alpineminirootfsfile="alpine-minirootfs-3.11.6-x86_64.tar.gz"
-linuxver="linux-5.6.8"
+alpineminirootfsfile="alpine-minirootfs-3.17.3-x86_64.tar.gz"
+linuxver="linux-6.3"
 
 wget -4 https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz
 mkdir alpine-minirootfs
 tar -C ./alpine-minirootfs -xf $alpineminirootfsfile
-wget -4 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.tar.xz
+wget -4 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.tar.xz
 tar -xf $linuxver.tar.xz
 
 ln -s $linuxver linux
