1509e7e4187405be46a5141aeffd53af6d7f1504
n30f0x <87524177+n30f0x@users.noreply.github.com>
2023-04-25 10:02:06 +0300

    updated minirootfs and add modern kernel
    i love hacking such stuff, very useful tool <3

--- FoxBuild/01_get.sh
+++ FoxBuild/01_get.sh
@@ -3,10 +3,10 @@
 alpineminirootfsfile="alpine-minirootfs-3.11.6-x86_64.tar.gz"
 linuxver="linux-5.6.8"
 
-wget -4 http://dl-cdn.alpinelinux.org/alpine/v3.11/releases/x86_64/$alpineminirootfsfile
+wget -4 https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz
 mkdir alpine-minirootfs
 tar -C ./alpine-minirootfs -xf $alpineminirootfsfile
-wget -4 https://cdn.kernel.org/pub/linux/kernel/v5.x/$linuxver.tar.xz
+wget -4 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.tar.xz
 tar -xf $linuxver.tar.xz
 
 ln -s $linuxver linux
