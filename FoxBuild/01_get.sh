0e5a21892b325b458528ef5972a436aacbce4317
br4wl0wl <ivan.d.mashkov@gmail.com>
2023-04-25 12:17:25 +0300

    another attempt

--- FoxBuild/01_get.sh
+++ FoxBuild/01_get.sh
@@ -4,7 +4,8 @@ alpineminirootfsfile="alpine-minirootfs-3.17.3-x86_64.tar.gz"
 linuxver="linux-6.3"
 
 wget -4 https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz
-tar -C ../alpine-minirootfs -xf $alpineminirootfsfile
+mkdir ../alpine-minirootfs
+tar -C ../alpine-minirootfs -xf $alpineminirootfsfile.tar.gz
 wget -4 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.tar.xz
 tar -xf ../$linuxver.tar.xz
 
