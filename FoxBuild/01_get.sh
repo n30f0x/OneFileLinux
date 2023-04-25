01ea3ebcd3b7c3440f6e53d2dd8d05cc5c2db29e
br4wl0wl <ivan.d.mashkov@gmail.com>
2023-04-25 11:52:30 +0300

    cleanup

--- FoxBuild/01_get.sh
+++ FoxBuild/01_get.sh
@@ -4,9 +4,8 @@ alpineminirootfsfile="alpine-minirootfs-3.17.3-x86_64.tar.gz"
 linuxver="linux-6.3"
 
 wget -4 https://dl-cdn.alpinelinux.org/alpine/v3.17/releases/x86_64/alpine-minirootfs-3.17.3-x86_64.tar.gz
-mkdir alpine-minirootfs
-tar -C ./alpine-minirootfs -xf $alpineminirootfsfile
+tar -C ../alpine-minirootfs -xf $alpineminirootfsfile
 wget -4 https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.3.tar.xz
-tar -xf $linuxver.tar.xz
+tar -xf ../$linuxver.tar.xz
 
 ln -s $linuxver linux
